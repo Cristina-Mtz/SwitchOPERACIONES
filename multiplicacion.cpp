c5763ecea3ae9a38f5c6ddba2c770d67d60d8581
Cristina Martinez <maria.martinez4724@alumnos.udg.mx>
2020-09-19 22:22:02 -0500

    Fuciona  codigo MULTIPLICACION

---
+++ multiplicacion.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+	{
+		int n1,n2,r;
+			printf("Inserta n1:\n");scanf("%d",&n1);
+			printf("Inserta n2:\n");scanf("%d",&n2);
+				r=n1*n2;
+			printf("El resultado es: %d\n",r);
+			system("pause");
+	}
