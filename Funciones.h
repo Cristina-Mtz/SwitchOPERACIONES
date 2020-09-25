6ca68118c69428015a0ecbb7d7fb7201deb36e83
Cristina Martinez <maria.martinez4724@alumnos.udg.mx>
2020-09-24 21:43:27 -0500

    Se agrega una funciona Funciones.h y se incorporan a multiplicaciones.cpp CODIGO FUNCIONAL

--- Funciones.h
+++ Funciones.h
@@ -1,22 +1,66 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
+#ifndef FUNCIONES
+#define FUNCIONES
+#include <stdio.h>
 
 
 void vector_enteros()
 	{
-		int vector[9],i,suma=0,prom=0;
-		for(i=0;i<=8;i++)
+		int n;
+			printf("Inserta el numero de enteros:\n");scanf("%d",&n);
+			system("cls");
+		int vector[n],i,suma=0;
+		float prom=0;
+		for(i=0;i<=n-1;i++)
 			{
-				printf("Ingresa el valor entero %d:\n",i);scanf("%d",&vector[i]);
+				printf("Ingresa el valor entero %d:\n",i+1);scanf("%d",&vector[i]);
 					suma=suma+vector[i];
 			}
-			prom=suma/9;
-		for(i=0;i<=8;i++)
+			prom=suma/n;
+		for(i=0;i<=n-1;i++)
 			{
-				printf("Posicion %d: %d\n",i,vector[i]);
-				printf("SUMA: %d\n", suma);
-				printf("PROMEDIO: %d\n",prom);
+				printf("Posicion %d: %d\n",i+1,vector[i]);
 			}		
+			printf("SUMA: %d\t\t", suma);
+			printf("PROMEDIO: %.2f\n",prom);
 	system("pause");					
+	}
+void registro()
+	{
+			int n;
+		printf("Inserta el numero de materias a registrar:\n");scanf("%d",&n);
+		system("cls");
+		struct 
+			{
+				char materia[100];
+				char maestro[100];
+				int horas;
+			} 
+		horario[n];
+
+				int i,ht=0;
+
+				for(i=0;i<=n-1;i++)
+					{
+						printf("Ingresa la materia %d:\n",i+1);fflush(stdin);gets(horario[i].materia);
+						printf("Ingresa el nombre del profesor %d:\n",i);fflush(stdin);gets(horario[i].maestro);
+						printf("Ingresa la cantidad de horas semanal %d:\n",i);scanf("%d",&horario[i].horas);
+							ht=ht+ horario[i].horas;
+						system("cls");
+					}
+					printf("MATERIA\t\t\t\t\tPROFESOR\t\t\t\tHORAS\n");
+					printf("_________________________________________________________________________________________\n");
+				for(i=0;i<=n-1;i++)
+					{
+						printf("%-25s\t\t",horario[i].materia);
+						printf("%-25s\t\t",horario[i].maestro);
+						printf("%d\t\t\t\t\n",horario[i].horas);
+						printf("_________________________________________________________________________________________\n");
+					}
+					printf("\t\t\t\t\t\t\t\tHORAS TOTALES:%d\n\n\n",ht);
+
+		
+		
+			
+		system("pause");
 	}
 #endif
--- multiplicacion.cpp
+++ multiplicacion.cpp
@@ -1,32 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "FUNCIONES.h"
 main()
 	{
-		int n1,n2,r,op;
-			printf("Selecciona una opcion:\n");
-			printf("1.SUMA\n2.MULTIPLICACION\n3.RESTA\n4.DIVISION\n");scanf("%d",&op);
-				switch(op)
-				{
-					case 1: printf("Inserta n1:\n");scanf("%d",&n1);
-							printf("Inserta n2:\n");scanf("%d",&n2);
-								r=n1+n2;
-							printf("El resultado es: %d\n",r);
-					break;
-					case 2: printf("Inserta n1:\n");scanf("%d",&n1);
-							printf("Inserta n2:\n");scanf("%d",&n2);
-								r=n1*n2;
-							printf("El resultado es: %d\n",r);
-					break;
-					case 3: printf("Inserta n1:\n");scanf("%d",&n1);
-							printf("Inserta n2:\n");scanf("%d",&n2);
-								r=n2-n1;
-							printf("El resultado es: %d\n",r);
-					break;
-					case 4: printf("Inserta n1:\n");scanf("%d",&n1);
-							printf("Inserta n2:\n");scanf("%d",&n2);
-								r=n1*n2;
-							printf("El resultado es: %d\n",r);
-					break;
-					default: printf("OPCION INVALIDA\n");
-				}
+		int s=0;
+			do
+			{
+				int n1,n2,r,op;
+					printf("Selecciona una opcion:\n");
+					printf("1.SUMA\n2.MULTIPLICACION\n3.RESTA\n4.DIVISION\n5.VECTOR\n6.HORARIO\n7.SALIR\n");scanf("%d",&op);
+						switch(op)
+						{
+							case 1: system("cls");
+									printf("Inserta n1:\n");scanf("%d",&n1);
+									printf("Inserta n2:\n");scanf("%d",&n2);
+										r=n1+n2;
+									printf("El resultado es: %d\n",r);
+							break;
+							case 2: system("cls");
+									printf("Inserta n1:\n");scanf("%d",&n1);
+									printf("Inserta n2:\n");scanf("%d",&n2);
+										r=n1*n2;
+									printf("El resultado es: %d\n",r);
+							break;
+							case 3: system("cls");
+									printf("Inserta n1:\n");scanf("%d",&n1);
+									printf("Inserta n2:\n");scanf("%d",&n2);
+										r=n2-n1;
+									printf("El resultado es: %d\n",r);
+							break;
+							case 4: system("cls");
+									printf("Inserta n1:\n");scanf("%d",&n1);
+									printf("Inserta n2:\n");scanf("%d",&n2);
+										r=n1*n2;
+									printf("El resultado es: %d\n",r);
+							break;
+							case 5: system("cls");
+									vector_enteros();
+							break;
+							case 6: system("cls");
+									registro( );
+							break;
+							case 7: s=1;
+							break;
+							default: printf("OPCION INVALIDA\n");
+						}
+				system("cls");
+			}
+			while(s==0);
+			system("pause");
 	}
